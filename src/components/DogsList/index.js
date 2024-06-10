f34f86da3564e814cb350e2ab6138ce60d26ccaa
HJELuis <lenrique.lejh@gmail.com>
2024-06-10 12:32:39 -0600

    getting dog info

---
+++ src/components/DogsList/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState, Fragment } from "react";
+import axios from "axios"
+
+const DogsList = () => {
+    const [dogs, setDogs] = useState([]);
+    const [error, setError] = useState([]);
+
+
+    useEffect(() =>{
+        const getDogs = async () => {
+            try{
+                const response = await axios.get("https://api.thedogapi.com/v1/breeds");                
+                setDogs(response.data);
+            }catch(error){
+                setError(error);
+            }            
+        }
+
+        getDogs();        
+    }, [])
+
+    return(
+        <Fragment>
+            <section> 
+           {                         
+                dogs.map((dog) => {                      
+                    return (
+                            <article key={dog.id}>
+                                <p>Name: {dog.name}</p>
+                                <p>Origin: {dog.origin}</p>
+                                <p>Life Span: {dog.life_span}</p>
+                                <img alt={`${dog.name}`} src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}></img>
+                            </article>
+                    );
+                })
+                
+            }
+            </section>
+            
+        </Fragment>
+    )
+
+
+}
+export default DogsList;
